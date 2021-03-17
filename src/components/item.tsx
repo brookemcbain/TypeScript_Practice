5f89581b02a5c27f219d8e2ac0328417382a4357
Brooke McBain <mcbainbrooke@gmail.com>
2021-03-16 20:33:18 -0500

    fake api information in

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -1,10 +1,24 @@
-import Button from '@material-ui/core/Button'
+import Button from "@material-ui/core/Button";
 //Types
-import {CartItemType} from '../App'
+import { CartItemType } from "../App";
 //Styles
-import {Wrapper} from './item.styles'
+import { Wrapper } from "./item.styles";
 
 type Props = {
-    item: CartItemType; 
-    handleAddToCart: 
-}
+  item: CartItemType;
+  handleAddToCart: (clickedItem: CartItemType) => void;
+};
+
+const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
+  <Wrapper>
+    <img src={item.image} alt={item.title} />
+    <div>
+      <h3>{item.title}</h3>
+      <p>{item.description}</p>
+      <h3>${item.price}</h3>
+    </div>
+    <Button onClick={() => handleAddToCart(item)}>Add to cart</Button>
+  </Wrapper>
+);
+
+export default Item;
