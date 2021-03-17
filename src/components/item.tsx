305ed8d0eb0a5b65a2a95fe5ce76f7b49888e02c
Brooke McBain <mcbainbrooke@gmail.com>
2021-03-16 20:22:10 -0500

    Added component folder for card component

---
+++ src/components/item.tsx
@@ -0,0 +1,10 @@
+import Button from '@material-ui/core/Button'
+//Types
+import {CartItemType} from '../App'
+//Styles
+import {Wrapper} from './item.styles'
+
+type Props = {
+    item: CartItemType; 
+    handleAddToCart: 
+}
